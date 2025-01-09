f51af41bbc5a9662719cf14b0f960a4a7b44de51
Mohamed Mahmoud <mohamed.haleem70@hotmail.com>
2025-01-09 16:00:41 +0200

    config color for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        PlusJakartaSans: ['Plus Jakarta Sans', 'ui-sans-serif', 'system-ui'], // Wrap in quotes
+        PlusJakartaSans: ["Plus Jakarta Sans", "ui-sans-serif", "system-ui"], // Wrap in quotes
       },
       backgroundImage: {
         "light-mode-bg": "url('./assets/images/bg-mobile-light.jpg')",
@@ -17,33 +14,34 @@ export default {
       },
       colors: {
         primary: {
-          brightBlue: "hsl(220, 98%, 61%)",
-          checkBackground:
-            "linear-gradient(to left,hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
+          brightBlue: "hsl(216, 100%, 40%)",
+          current: "#22C55E",
+          doing: "#EF4444",
+          done: "#3B82F6",
         },
         neutral: {
           // Light theme colors
           light: {
-            veryLightGray: "hsl(0, 0%, 98%)",
-            veryLightGrayishBlue: "hsl(236, 33%, 92%)",
-            lightGrayishBlue: "hsl(233, 11%, 84%)",
+            mainText: "black",
+            ContentText: "#6b7280",
+            veryLightSideNavCards: "hsl(0, 0%, 98%)",
+            veryLightMain: "#F4F7FD",
+            lightNewColumn: "#E9EFFA",
             darkGrayishBlue: "hsl(236, 9%, 61%)",
             veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
           },
           //dark theme color
           dark: {
-            veryDarkBlue: "hsl(235, 21%, 11%)",
-            veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
-            lightGrayishBlue: "hsl(234, 39%, 85%)",
-            lightGrayishBlueHover: "hsl(236, 33%, 92%)",
-            darkGrayishBlue: "hsl(234, 11%, 52%)",
-            veryDarkGrayishBlue1: "hsl(233, 14%, 35%)",
-            veryDarkGrayishBlue2: "hsl(237, 14%, 26%)",
+            veryDarkBlueSideNavCards: "#2B2C37",
+            veryDarkMain: "#20212C",
+            lightText: "hsl(0, 0%, 98%)",
+            darkCardText: "#6B7780",
+            backgroundSideNav: "#635fc7",
+            backgroundSideNavhover: "#f4f5f7",
           },
         },
       },
     },
   },
   plugins: [],
-}
-
+};
