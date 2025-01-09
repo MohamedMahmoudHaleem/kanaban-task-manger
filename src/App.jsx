52e7248cae5cc96f3ad9c2f76306b9aec1b07df7
Mohamed Mahmoud <mohamed.haleem70@hotmail.com>
2025-01-09 14:07:34 +0200

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 export default function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline text-red-900 bg-blue-500 ">Hello world!</h1>
+    <p className='text-2xl' >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, molestias. Omnis, porro vitae? Illum neque repudiandae magni, nam pariatur, dolorem non iusto cumque veniam sit molestias quaerat aut mollitia. Aspernatur.</p>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,39 @@ export default {
       fontFamily: {
         PlusJakartaSans: ['Plus Jakarta Sans', 'ui-sans-serif', 'system-ui'], // Wrap in quotes
       },
+      backgroundImage: {
+        "light-mode-bg": "url('./assets/images/bg-mobile-light.jpg')",
+        "light-mode-bg-desktop": "url('./assets/images/bg-desktop-light.jpg')",
+        "dark-mode-bg": "url('./assets/images/bg-mobile-dark.jpg')",
+        "dark-mode-bg-desktop": "url('./assets/images/bg-desktop-dark.jpg')",
+      },
+      colors: {
+        primary: {
+          brightBlue: "hsl(220, 98%, 61%)",
+          checkBackground:
+            "linear-gradient(to left,hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
+        },
+        neutral: {
+          // Light theme colors
+          light: {
+            veryLightGray: "hsl(0, 0%, 98%)",
+            veryLightGrayishBlue: "hsl(236, 33%, 92%)",
+            lightGrayishBlue: "hsl(233, 11%, 84%)",
+            darkGrayishBlue: "hsl(236, 9%, 61%)",
+            veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+          },
+          //dark theme color
+          dark: {
+            veryDarkBlue: "hsl(235, 21%, 11%)",
+            veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+            lightGrayishBlue: "hsl(234, 39%, 85%)",
+            lightGrayishBlueHover: "hsl(236, 33%, 92%)",
+            darkGrayishBlue: "hsl(234, 11%, 52%)",
+            veryDarkGrayishBlue1: "hsl(233, 14%, 35%)",
+            veryDarkGrayishBlue2: "hsl(237, 14%, 26%)",
+          },
+        },
+      },
     },
   },
   plugins: [],
