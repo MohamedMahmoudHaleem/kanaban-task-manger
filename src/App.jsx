49b7b64935eb1fc5647c8a1a27afa2ae7b328b3b
Mohamed Mahmoud <mohamed.haleem70@hotmail.com>
2025-01-09 13:45:12 +0200

    fix tailwind issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      {" "}
-      <h1 className="text-red-500 font-PlusJakartaSans">hello</h1>
-      <h1>Plus Jakarta Sans</h1>
-      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-      <p>123456790</p>
-      <p>ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
-      <p>abcdefghijklmnopqrstuvwxyz</p>
-    </>
+    <h1 className="text-3xl font-bold underline text-red-900 bg-blue-500 ">Hello world!</h1>
   );
 }
-
-export default App;
