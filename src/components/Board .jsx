300b187d25923d38c34fe04dd1b3665bf5a579c2
Mohamed Mahmoud <mohamed.haleem70@hotmail.com>
2025-01-11 22:39:47 +0200

    fix:style Create New Board

--- src/components/Board .jsx
+++ src/components/Board .jsx
@@ -2,15 +2,15 @@ function Board() {
   return (
     <div className="mt-[92px] bg-neutral-light-veryLightMain w-full  ">
       <div
-        className="min-h-full mt-16 flex flex-col items-center justify-center gap-4
+        className="min-h-full mt-14 flex flex-col items-center justify-center gap-4
         "
       >
         <p className="font-medium text-base text-center ">
           This board is empty. Create a new column to get started.
         </p>
         <button
-          className="flex flex-row items-center justify-start gap-3 py-3 px-4 
-        btn  text-neutral-light-veryLightSideNavCards font-semibold hover:transition-all  hover:duration-150  "
+        className="flex flex-row items-center justify-start py-3 px-5 
+        btn text-neutral-light-veryLightSideNavCards font-semibold hover:transition-all hover:duration-150  "
         >
           + Create New Board
         </button>
