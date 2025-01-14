210886dd0c0898c7d33143ef8fc43e0ff60d1728
Mohamed Mahmoud <mohamed.haleem70@hotmail.com>
2025-01-14 16:18:49 +0200

    style: board page

--- src/pages/Board .jsx
+++ src/pages/Board .jsx
@@ -3,15 +3,16 @@ import { kanbanContext } from "../Context/kanbanContext.jsx";
 import AddNewBoard from "../layouts/MainLayout/components/NavBar/AddNewBoard.jsx";
 
 function Board() {
-  const { openNewBoard, handleOpenBoard, handleCloseBoard } =
+  const { openNewBoard, handleOpenBoard, handleCloseBoard, boards } =
     useContext(kanbanContext);
+  console.log("boards in  ", boards);
   return (
-    <div className="mt-[92px] bg-neutral-light-veryLightMain w-full  ">
+    <div className="mt-20 min-h-full bg-neutral-light-veryLightMain w-full  ">
       <div
-        className="min-h-full mt-10 flex flex-col items-center justify-center gap-4
+        className="min-h-full  flex flex-col items-center  justify-center gap-4
         "
       >
-        <p className="font-medium text-base text-center ">
+        {/* <p className="font-medium text-base text-center ">
           This board is empty. Create a new column to get started.
         </p>
         <button
@@ -20,8 +21,24 @@ function Board() {
           onClick={handleOpenBoard}
         >
           + Create New Board
-        </button>
+        </button> */}
+
+        <div className="min-h-[75vh] flex flex-row gap-11 self-start mx-11 items-center">
+          <div className="flex flex-col gap-3">
+            <div className="flex flex-row gap-2 justify-start items-center">
+              <span className="rounded-full w-3 h-3 bg-red-900"></span>
+              <span className="text-[#a3acba] font-semibold tracking-wide">ONe</span>
+            </div>
+            <div className="w-72 min-h-[75vh] border-[2px] border-opacity-60 border-neutral-light-darkGrayishBlue border-dashed rounded-md "></div>
+          </div>
+          <div className="w-72 min-h-[75vh] self-stretch mt-9 from-[#cad5e5] to-slate-100">
+            <button className="w-full min-h-full rounded-md bg-gradient-to-b text-[#4e545e] font-bold text-2xl text-opacity-60 hover:text-primary-brightBlue ">
+              + New Column
+            </button>
+          </div>
+        </div>
       </div>
+
       {openNewBoard && (
         <AddNewBoard isOpen={openNewBoard} handleClose={handleCloseBoard} />
       )}
