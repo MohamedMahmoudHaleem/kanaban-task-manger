865793d0f5d4ab95c3baf79212d0351cac9de475
Mohamed Mahmoud <mohamed.haleem70@hotmail.com>
2025-01-14 16:59:23 +0200

    refactor : restructure the context provider in mainLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,18 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 import { useState } from "react";
-import { kanbanContext } from "./Context/kanbanContext.jsx";
 import MainLayout from "./layouts/MainLayout/MainLayout.jsx";
 import Board from "./pages/Board .jsx";
 
 export default function App() {
-  //toggle sidebar
-  const [openSideBar, setOpenSideBar] = useState(true);
-  function handleOpenSideBar() {
-    setOpenSideBar(!openSideBar);
-  }
-  //Create New Board .
-  const [openNewBoard, setAddNewBoard] = useState(false);
-  const handleOpenBoard = () => {
-    setAddNewBoard(true);
-  };
-  const handleCloseBoard = () => {
-    setAddNewBoard(false);
-  };
-
-  //add newBoard data
-  const [boards, setBoards] = useState(
-    JSON.parse(localStorage.getItem("newBoard")) || []
-  );
+  const queryClient = new QueryClient();
 
   return (
-    <kanbanContext.Provider
-      value={{
-        openSideBar,
-        handleOpenSideBar,
-        openNewBoard,
-        handleOpenBoard,
-        handleCloseBoard,
-        boards,
-        setBoards,
-      }}
-    >
-      <MainLayout>
-        <Board />
-      </MainLayout>
-    </kanbanContext.Provider>
+    <MainLayout>
+      <Board />
+    </MainLayout>
   );
 }
--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import SideBar from "./components/SideBar/SideBar.jsx";
+import { kanbanContext } from "../../Context/kanbanContext.jsx";
 
 function MainLayout({ children }) {
+  //toggle sidebar
+  const [openSideBar, setOpenSideBar] = useState(true);
+  function handleOpenSideBar() {
+    setOpenSideBar(!openSideBar);
+  }
+  //Create New Board .
+  const [openNewBoard, setAddNewBoard] = useState(false);
+  const handleOpenBoard = () => {
+    setAddNewBoard(true);
+  };
+  const handleCloseBoard = () => {
+    setAddNewBoard(false);
+  };
+
+  //add newBoard data
+  const [boards, setBoards] = useState(
+    JSON.parse(localStorage.getItem("newBoard")) || []
+  );
   return (
-    <div>
-      <NavBar />
-      <div className="flex flex-row min-h-screen overflow-hidden">
-        <SideBar />
-        {children}
+    <kanbanContext.Provider
+      value={{
+        openSideBar,
+        handleOpenSideBar,
+        openNewBoard,
+        handleOpenBoard,
+        handleCloseBoard,
+        boards,
+        setBoards,
+      }}
+    >
+      <div>
+        <NavBar />
+        <div className="flex flex-row min-h-screen overflow-hidden">
+          <SideBar />
+          {children}
+        </div>
       </div>
-    </div>
+    </kanbanContext.Provider>
   );
 }
 
