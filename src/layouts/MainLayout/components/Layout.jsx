6d114e745fabdf09cf70cdd7618c393d48ccedb9
Mohamed Mahmoud <mohamed.haleem70@hotmail.com>
2025-01-19 01:02:22 +0200

    feat  : add useEffect to the layout to delete the params

--- src/layouts/MainLayout/components/Layout.jsx
+++ src/layouts/MainLayout/components/Layout.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useKanban } from "../../../Context/kanbanContext.jsx";
 import Board from "../../../pages/Board/Board .jsx";
 import NavBar from "./NavBar/NavBar.jsx";
@@ -6,6 +7,14 @@ import SideBar from "./SideBar/SideBar.jsx";
 
 function Layout() {
   const { openSideBar } = useKanban();
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    // Check if the URL contains the 'id' parameter and remove it
+    if (url.searchParams.has("id")) {
+      url.searchParams.delete("id");
+      window.history.replaceState({}, "", url.toString());
+    }
+  }, []);
   return (
     <div>
       <NavBar />
