83ff61be3ad4e9972a5350e6738a1e2ebc6792a6
Mohamed Mahmoud <mohamed.haleem70@hotmail.com>
2025-01-19 01:00:40 +0200

    refactor : edit delete boards

--- src/layouts/MainLayout/components/Utilitycomponents/EditedBoard.jsx
+++ src/layouts/MainLayout/components/Utilitycomponents/EditedBoard.jsx
@@ -1,13 +1,31 @@
+// import { useToggle } from "../../../../components/UseToggle.jsx";
 import { useKanban } from "../../../../Context/kanbanContext.jsx";
 
 function EditedBoard() {
-  // function stopPropagations(e){
-  //     // e.traget.
-  // }
+
+  
+  const { setBoards, boards, handleCloseEditCard } = useKanban();
+  console.log("✌️ ~ EditedBoard ~ boards:", boards);
+
+  function deleteBoards() {
+    const url = new URL(window.location.href); // Get the current search parameters
+    const deletedId = url.searchParams.get("id");
+    console.log("✌️ ~ deleteBoards ~ id:", deletedId);
+    setBoards((boards) =>
+      boards.filter((board) => board.id.toString() !== deletedId.toString())
+    );
+    // Change URL to the initial state without 'id'
+    url.searchParams.delete("id");
+    window.history.pushState({}, "", url.toString());
+    handleCloseEditCard();
+  }
+
   return (
     <div className="absolute top-[90px] right-10 py-4 rounded-md bg-neutral-light-veryLightSideNavCards w-48 flex flex-col items-start px-4 gap-2 shadow-md">
       <button className=" hover:opacity-50">Edit Board</button>
-      <button className="text-red-600 hover:opacity-50 ">Delete Board</button>
+      <button className="text-red-600 hover:opacity-50" onClick={deleteBoards}>
+        Delete Board
+      </button>
     </div>
   );
 }
